8fedc4112671ad16b9feb79a9fdd2e6de62d0e50
Sumit <sk990005@outlook.com>
2020-06-01 12:58:05 +0530

    backend initial commit

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ require("./startup/logging")();
 require("./services/scheduler")();
 require("./startup/routes")(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 // PORT setting
 const port = process.env.PORT || 5000;
