7fe73155bcbbd32d9d01ca9218aaf43140b35bf9
Sumit <sk990005@outlook.com>
2020-06-02 16:53:41 +0530

    backend commit

--- startup/routes.js
+++ startup/routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+const request = require("request");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
 const reset = require("../routes/reset");
@@ -9,6 +9,11 @@ const todos = require("../routes/todos");
 const error = require("../middleware/error");
 
 module.exports = function (app) {
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+  });
+
   app.use(express.json());
   app.use("/register", users);
   app.use("/auth", auth);
@@ -16,5 +21,20 @@ module.exports = function (app) {
   app.use("/categories", categories);
   app.use("/todos", todos);
   app.use("/feedback", feedbacks);
+
+  app.get("/countries", (req, res) => {
+    request(
+      {
+        url: "https://restcountries.eu/rest/v2/all",
+      },
+      (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          return res.status(500).json({ type: "error", message: err.message });
+        }
+
+        res.json(JSON.parse(body));
+      }
+    );
+  });
   app.use(error);
 };
